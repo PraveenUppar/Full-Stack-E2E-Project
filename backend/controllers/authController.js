f6da1d801a81941d0626ac5b6fd6f18e1b531f94
Praveen Uppar <praveenuppar718@gmail.com>
2025-09-14 22:25:32 +0530

    auth setup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,27 @@
-const loginUser
+import User from "../models/userModel.js";
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+  res.status(201).json({ message: "User registered successfully" });
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  res.status(200).json({ message: "User logged in successfully" });
+};
+
+export { registerUser, loginUser };
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -13,9 +20,10 @@ app.get("/", (req, res) => {
 import authRoutes from "./routes/authRoutes.js";
 app.use("/api/auth", authRoutes);
 
-import todoRoutes from "./routes/todoRoutes.js";
-app.use("/api/todo", todoRoutes);
+// import todoRoutes from "./routes/todoRoutes.js";
+// app.use("/api/todo", todoRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  connectDB();
+  console.log(`Server listening on port ${port}`);
 });
