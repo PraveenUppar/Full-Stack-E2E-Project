bd214472ca6702d5c7d193589ab7e050920e8fb4
Praveen Uppar <praveenuppar718@gmail.com>
2025-09-14 22:37:12 +0530

    webtokens

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -6,9 +7,19 @@ const registerUser = async (req, res) => {
   if (userExists) {
     return res.status(400).json({ message: "User already exists" });
   }
-  const newUser = new User({ name, email, password });
-  await newUser.save();
-  res.status(201).json({ message: "User registered successfully" });
+  const user = await User.create({ name, email, password });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({ message: "User registered successfully" });
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Credentials");
+  }
 };
 
 const loginUser = async (req, res) => {
@@ -17,11 +28,18 @@ const loginUser = async (req, res) => {
   if (!user) {
     return res.status(400).json({ message: "Invalid credentials" });
   }
-  const isMatch = await user.matchPassword(password);
-  if (!isMatch) {
-    return res.status(400).json({ message: "Invalid credentials" });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({ message: "User logged in successfully" });
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error(" Invalid email or password");
   }
-  res.status(200).json({ message: "User logged in successfully" });
 };
 
 export { registerUser, loginUser };
