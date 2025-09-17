ca8342cc64877bdf2e133769328dfcd4e7df272b
Praveen Uppar <praveenuppar718@gmail.com>
2025-09-17 18:20:37 +0530

    retry

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 function Header() {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
-
   return (
     <header className="bg-black border-b-2 border-white p-2">
       <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
@@ -17,12 +12,6 @@ function Header() {
         >
           Task Manager
         </Link>
-        <button
-          onClick={handleLogout}
-          className="bg-white px-4 font-mono text-lg py-2 rounded hover:bg-red-500"
-        >
-          Logout
-        </button>
       </nav>
     </header>
   );
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = process.env.VITE_API_URL || "http://localhost:3000";
+export const BACKEND_URL = "http://localhost:5000";
